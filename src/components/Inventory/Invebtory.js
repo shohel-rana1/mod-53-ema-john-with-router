ad763970e6fa174abb28be843ab7aaccf3d06d02
shohel-rana1 <gmshohel53@gmail.com>
2021-10-03 21:10:51 +0600

    added Inventory

---
+++ src/components/Inventory/Invebtory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Invebtory = () => {
+    return (
+        <div>
+            <h2>This is Inventory</h2>
+        </div>
+    );
+};
+
+export default Invebtory;
